ea6805964ae53dd317ed530d76801f392997f3d8
Vesko B <vese4oka@gmail.com>
2018-12-11 17:49:16 +0200

    proba

--- Calc/ViewController.swift
+++ Calc/ViewController.swift
@@ -9,10 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    enum Operator {
+        case plus
+        case minus
+        case multiply
+        case devide
+        case percentage
+    }
     @IBOutlet weak var display: UILabel!
     var a = 0
     var b = 0
-    
+    var op : Operator = Operator.plus
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,7 +28,25 @@ class ViewController: UIViewController {
     }
 
     @IBAction func clickEquals(_ sender: Any) {
-        let result = a+b
+        let result: Int
+        switch op {
+        case .plus:
+            result = a + b
+        case .minus:
+            result = a - b
+        case .multiply:
+            result = a * b
+        case .devide:
+            if b == 0 {
+                display.text = "Error"
+                return
+            } else {
+                result = a / b
+            }
+        case .percentage:
+            result = a / b
+            
+        }
         display.text = String(result)
     }
     
@@ -38,7 +64,76 @@ class ViewController: UIViewController {
         a = b
         b = 0
         display.text = String(b)
+        op = Operator.plus
+    }
+    
+    @IBAction func clickThree(_ sender: Any) {
+        b = 3
+        display.text = String(b)
+    }
+    
+    @IBAction func clickFour(_ sender: Any) {
+        b = 4
+        display.text = String(b)
+    }
+    
+    @IBAction func clickFive(_ sender: Any) {
+        b = 5
+        display.text = String(b)
+    }
+    
+    @IBAction func clickSix(_ sender: Any) {
+        b = 6
+        display.text = String(b)
+    }
+    
+    @IBAction func clickSeven(_ sender: Any) {
+        b = 7
+        display.text = String(b)
+    }
+    
+    @IBAction func clickMinus(_ sender: Any) {
+        a = b
+        b = 0
+        op = Operator.minus
+     
+    }
+    
+    @IBAction func clickEight(_ sender: Any) {
+        b = 8
+        display.text = String(b)
+        
+    }
+    
+    @IBAction func clickNine(_ sender: Any) {
+        b = 9
+        display.text = String(b)
+    }
+    
+    @IBAction func clickMultiply(_ sender: Any) {
+        a = b
+        b = 0
+        op = Operator.multiply
+    }
+    
+    @IBAction func clickDevide(_ sender: Any) {
+        a = b
+        b = 0
+        op = Operator.devide
+    }
+    
+    @IBAction func clickZero(_ sender: Any) {
+        b = 0
+        display.text = String(b)
+    }
+    
+    @IBAction func clickPercentage(_ sender: Any) {
+        a = b
+        b = Int(0.01)
+        display.text = String(b)
+        op = Operator.percentage
     }
     
 }
+
 
